0a4f23545d5a2a51114b4a25dbe115d87c5faa7d
katie li <katie31@berkeley.edu>
2016-09-28 01:03:09 -0700

    Finished hw1

--- foobar.rb
+++ foobar.rb
@@ -3,6 +3,7 @@ class Foobar
   def self.baz(a)
     # Class method
     # Call with `Foobar.baz`
+    (a.map!{|x| x.to_i}).map!{|x| x + 2}.select{|x| x%2 ==0}.collect{|x| x}.uniq.select{|x| x<=10}.collect{|x| x}.inject(0,:+)
   end
 end
 
